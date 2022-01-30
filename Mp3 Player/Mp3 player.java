1f9fa1aa7127655116623c915b395b5eb69be82c
Amogh Thusoo <98604199+amoghthusoo@users.noreply.github.com>
2022-01-31 01:19:30 +0530

    Add files via upload

---
+++ Mp3 Player/Mp3 player.java
@@ -0,0 +1,33 @@
+// Program to play Mp3 file format
+// Requires "jl1.0.jar"
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import javazoom.jl.player.Player;
+
+class Mp3Player {
+
+    private String filename;
+    private Player player;
+
+    public Mp3Player(String filename) {
+        this.filename = filename;
+    }
+
+    public void play() {
+        try {
+            BufferedInputStream buffer = new BufferedInputStream(new FileInputStream(filename));
+            player = new Player(buffer);
+            player.play();
+        } catch (Exception e) {
+
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Mp3Player mp3 = new Mp3Player("monody.mp3");
+        mp3.play();
+    }
+}
